404f31abf1b8879cb78e565175e449030f860782
Han-zhifeng <zhifeng@hanzhifengdeMacBook-Pro.local>
2016-05-25 17:47:53 +0800

    SwiftLearn-Shopping
    learn swift
    test project shopping

---
+++ Shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by 韩志峰 on 16/5/25.
+//  Copyright © 2016年 Zhifeng Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
